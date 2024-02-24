561238609e70d12fbe04f075c9cf1f3296a8f6c2
Luis Vigoya <vigoya-2306@hotmail.com>
2024-02-23 22:15:49 -0500

    Terminado hasta la clase 66

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-add-character',
+  templateUrl: './add-character.component.html',
+  styles: [
+  ]
+})
+export class AddCharacterComponent {
+
+}
